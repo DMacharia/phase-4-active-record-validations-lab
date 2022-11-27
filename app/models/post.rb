66d86383a5e04ae756f38f322a7a1d669a23adc2
Daniel Macharia <danielhuios@gmail.com>
2022-11-27 15:32:22 +0300

    completed record validation lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :title_clickbait
+
+    def title_clickbait
+        if !(["Won't Believe","Secret","Top [number]","Guess"].any?{|phrase| "#{title}".include?(phrase)})
+          errors.add(:title, "not clickbaity")
+        end
+      end
 end
